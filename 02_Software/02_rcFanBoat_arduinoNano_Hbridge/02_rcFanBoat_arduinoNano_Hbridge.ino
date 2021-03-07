d9f2a9156f9883c1696ea5a744a484e42b977d30
Timo <thundy@web.de>
2021-03-07 18:22:55 +0100

    designfreeze powerstage channels
    freeze design: powerstage channels
    update pinout accordingly
    adapt SW accordingly (fix forward mode)

--- 02_Software/02_rcFanBoat_arduinoNano_Hbridge/02_rcFanBoat_arduinoNano_Hbridge.ino
+++ 02_Software/02_rcFanBoat_arduinoNano_Hbridge/02_rcFanBoat_arduinoNano_Hbridge.ino
@@ -3,25 +3,29 @@
 int ppmSigLastPosEdge_micros = 0;
 int ppmSignalPulseWidth_micros= 0;
 const byte interruptPin = 2;
-int ppmSig_lowVal = 996;
-int ppmSig_upMinLowVal = 1996;
+int ppmSig_lowVal = 990;
+int ppmSig_upMinLowVal = 1984;
 long ppmSig_min1000 = 0;
 long ppmSig_max2000 = 0;
 boolean forwardDirectionState = false; // false: such that positive edge is provoked and switches to forward mode initially
 boolean directionForwardRequest = true;
+uint8_t testState = 0;
 
 // signal for PWM generation
 int sensorValue = 0; // test-signal from poti
 int fanPWMSig_fromPPMSig = 0;
 
-#define Hpositive_ON TCCR1A |= (1 << COM1A1)
-#define Hnegative_ON TCCR1A |= (1 << COM1B1)
-#define Hpositive_OFF TCCR1A &= ~(1 << COM1A1)
-#define Hnegative_OFF TCCR1A &= ~(1 << COM1B1)
-#define Lpositive_ON PORTB |= (1 << PORTB0)
-#define Lpositive_OFF PORTB &= ~(1 << PORTB0)
-#define Lnegative_ON PORTD |= (1 << PORTD7)
-#define Lnegative_OFF PORTD &= ~(1 << PORTD7)
+#define Lpositive_ON TCCR1A |= (1 << COM1B1)
+#define Lpositive_OFF TCCR1A &= ~(1 << COM1B1)
+
+#define Lnegative_ON TCCR1A |= (1 << COM1A1)
+#define Lnegative_OFF TCCR1A &= ~(1 << COM1A1)
+
+#define Hpositive_ON PORTB |= (1 << PORTB0)
+#define Hpositive_OFF PORTB &= ~(1 << PORTB0)
+
+#define Hnegative_ON PORTD |= (1 << PORTD7)
+#define Hnegative_OFF PORTD &= ~(1 << PORTD7)
 
 void setup() {
   Serial.begin(9600);
@@ -30,10 +34,10 @@ void setup() {
 
   // setup PWM 
   TCCR1A = 0; // reset all registers
-  DDRB = (1 << DDB1);      // PB1 is now an output for OC1A -> H+  (D9)
-  DDRB |= (1 << DDB2);      // PB2 is now an output for OC1B -> H-  (D10)
-  DDRB |= (1 << DDB0);      // PB0 is now an output -> L-  (D8)
-  DDRD = (1 << DDD7);      // PD7 is now an output -> L+  (D7)
+  DDRB = (1 << DDB2);     // PB2 is now an output for OC1B -> L+  (D10)
+  DDRB |= (1 << DDB1);    // PB1 is now an output for OC1A -> L-  (D9)
+  DDRB |= (1 << DDB0);    // PB0 is now an output -> H+  (D8)
+  DDRD = (1 << DDD7);     // PD7 is now an output -> H-  (D7)
 
   // set up PWM outputs
   TCCR1A |= (1 << WGM10);  // set phase correct Mode (takes double the time for a PWM cycle)
@@ -42,16 +46,10 @@ void setup() {
   OCR1B = 0x00;            // set PB2 PWM for 50% duty cycle
 
   // turn off powerstage completely in the beginning
-  /*
   Hpositive_OFF;
   Hnegative_OFF;
   Lpositive_OFF;
   Lnegative_OFF;
-  */
-  Hpositive_OFF;
-  Lnegative_OFF;
-  Hnegative_ON;
-  Lpositive_ON;
 }
 
 void loop() {  
@@ -65,6 +63,7 @@ void loop() {
     Hpositive_ON;
     Lnegative_ON;
     forwardDirectionState = directionForwardRequest; // update state
+    testState = 1;
   }
   else if( (forwardDirectionState==true) && (directionForwardRequest==false) ){
     // switching to backward mode
@@ -74,19 +73,22 @@ void loop() {
     Hnegative_ON;
     Lpositive_ON;
     forwardDirectionState = directionForwardRequest; // update state
+    testState = 2;
   }
   
   // make sure the PPM pulse duration has its lowest value at "ppmSig_lowVal" and has a maximum value add of 1000µs
   ppmSig_min1000 = (long)min(max(ppmSignalPulseWidth_micros-ppmSig_lowVal,0),1000);
   // now make sure that the PPM pulse is not bigger than "ppmSig_upMinLowVal"
-  ppmSig_max2000 = min(ppmSig_min1000+(ppmSig_upMinLowVal-ppmSig_lowVal),(long)2000);
+  ppmSig_max2000 = min(ppmSig_min1000+ppmSig_lowVal-1000+ppmSig_upMinLowVal,2000);
 
   // scale ppmSig_min1000 range of 0..1000 down to 0..255
   //ppmSig_max2000 = (ppmSig_min1000 *(long)255) / (long)1000;
   OCR1A = min( (uint8_t)( (ppmSig_min1000 *(long)255) / (long)1000), 255);
+  OCR1B = OCR1A; // forward and backward have the same duty cycle (distinguished by forward-backward-bit)
+  
   Serial.print(ppmSignalPulseWidth_micros,DEC);
   Serial.print(", ");
-  Serial.print(ppmSig_min1000,DEC);
+  Serial.print(testState,DEC);  
   Serial.print(", ");
   Serial.println(OCR1A,DEC);
 }
